6def4b5e62093fff08a77b181d552570bdea8a4d
Kelly Lougheed <kellylougheed@gmail.com>
2025-12-12 02:29:29 +0000

    changing cookie

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true, // HTTPS on Render
     sameSite: 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days
   }
